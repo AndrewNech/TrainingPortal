e8f353aa6ab3ff080353f16d16840bd75074e90d
AndrewNech <Drew.Fenix@yandex.ru>
2018-05-03 16:04:10 +0300

    tp-2.1 add structureMap

---
+++ TrainingPortal/TrainigPortal.App.General/Loggers/Concrete/LoggerWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainigPortal.App.General.Loggers.Abstract;
+
+namespace TrainigPortal.App.General.Loggers
+{
+    public class LoggerWrapper : ILoggerWrapper
+    {
+    }
+}
